15200f267fc746be98ad3af9b1f2f4adc1a51193
Jonathan Cain <xivdot@gmail.com>
2017-05-08 18:40:06 -0700

    Added logged on user check

---
+++ Infrastructure/WmiServices.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Management;
+
+namespace DeviceMonitor.Infrastructure
+{
+    public class WmiServices
+    {
+        private static string RootNamespace => "\\root\\cimv2";
+
+        public static ManagementScope ConnectToRemoteWmi(string hostname, ConnectionOptions options)
+        {
+            try
+            {
+                var wmiscope = new ManagementScope($"\\\\{hostname}{RootNamespace}", options);
+                wmiscope.Connect();
+                return wmiscope;
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+
+        public static string QueryLoggedOnUser(string device)
+        {
+            var result = "";
+            var remote = ConnectToRemoteWmi(device, new ConnectionOptions());
+            if (remote == null) return result;
+
+            var query = new ObjectQuery("SELECT username FROM Win32_ComputerSystem");
+
+            var searcher = new ManagementObjectSearcher(remote, query);
+            var queryCollection = searcher.Get();
+
+            foreach (var resultobject in queryCollection)
+            {
+                result = $"{resultobject["username"]}";
+            }
+
+            return result;
+        }
+    }
+}
